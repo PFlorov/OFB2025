6b8944f5c63a52a31e403e67f1c608870a43225a
PFlorov <plamengerasimov99@gmail.com>
2025-05-19 15:07:08 +0300

    hw1 task4

--- src/Homework1.java
+++ src/Homework1.java
@@ -52,5 +52,46 @@ public class Homework1 {
         scanner.close();
     }
 
+    public static  void task4(){
+            Scanner scanner = new Scanner(System.in);
+
+            double puzzlePrice = 2.60;
+            int talkingDollPrice = 3;
+            double teddyBearPrice = 4.10;
+            double minionPrice = 8.20;
+            int truckPrice = 2;
+
+            double tripCost = scanner.nextDouble();
+            int numberOfPuzzles = scanner.nextInt();
+            int numberOfTalkingDolls = scanner.nextInt();
+            int numberOfTeddyBears = scanner.nextInt();
+            int numberOfMinions = scanner.nextInt();
+            int numberOfTrucks = scanner.nextInt();
+
+            int totalToys = numberOfPuzzles + numberOfTalkingDolls + numberOfTeddyBears + numberOfMinions + numberOfTrucks;
+            double totalCostOfOrder = numberOfPuzzles * puzzlePrice +
+                    numberOfTalkingDolls * talkingDollPrice +
+                    numberOfTeddyBears * teddyBearPrice +
+                    numberOfMinions * minionPrice +
+                    numberOfTrucks * truckPrice;
+            if(totalToys >= 50){
+                totalCostOfOrder = totalCostOfOrder * 0.75;
+
+            }
+
+            double moneyLeftAfterRent = totalCostOfOrder * 0.90;
+
+
+            if (moneyLeftAfterRent > tripCost){
+                moneyLeftAfterRent -= tripCost;
+                System.out.printf("Yes! %.2f lv left.",moneyLeftAfterRent );
+            }
+            else{
+                tripCost -= moneyLeftAfterRent;
+                System.out.printf("Not enough money! %.2f lv needed.",tripCost);
+            }
+
+    }
+
 
 }
